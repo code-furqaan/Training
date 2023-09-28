44a2a525aea481221a3189773d563320743224ae
Furqaan <code.furqaan@gmail.com>
2023-09-28 15:00:04 +0530

    commit_28_9_2

---
+++ Python/Day 6/linked_list final.py
@@ -0,0 +1,142 @@
+class Node:
+    def __init__(self, data):
+        self.__data = data
+        self.__next = None
+
+    def get_next(self):
+        return self.__next
+    
+    def set_next(self, node):
+        self.__next = node
+    
+    def get_data(self):
+        return self.__data
+    
+    def set_data(self, data):
+        self.__data = data
+
+class Linked_List:
+    def __init__(self, node):
+        self.__node = node
+    
+    def update_first(self, old_data, new_data):
+        temp = self.__node
+        flag = False
+
+        if temp.get_data() == old_data:
+            temp.set_data(new_data)
+            flag = True
+        else:
+            while temp.get_next() != None:
+                if temp.get_data() == old_data:
+                    temp.set_data(new_data)
+                    flag = True
+                    break
+                temp = temp.get_next()
+
+            if temp.get_data() == old_data:
+                temp.set_data(new_data)
+                flag = True
+        if flag:
+            print(f"First {old_data} updated to {new_data}")
+        else:
+            print(f"{old_data} not found")
+    
+    def update_all(self, old_data, new_data):
+        temp = self.__node
+        flag = False
+
+        if temp.get_data() == old_data:
+            temp.set_data(new_data)
+            flag = True
+        else:
+            while temp.get_next() != None:
+                if temp.get_data() == old_data:
+                    temp.set_data(new_data)
+                    flag = True
+                temp = temp.get_next()
+
+            if temp.get_data() == old_data:
+                temp.set_data(new_data)
+                flag = True
+        if flag:
+            print(f"All {old_data} updated to {new_data}")
+        else:
+            print(f"{old_data} not found")
+    
+    def append(self, node):
+        temp = self.__node
+        # print(node.__data)
+        while temp.get_next() != None:
+            temp = temp.get_next()
+            # print(node.__data)    
+        temp.set_next(node)
+        print(f"{node.get_data()} appended")
+    
+    def print_list(self):
+        temp = self.__node
+        while temp.get_next() != None:
+            print(temp.get_data(), end=' --> ')
+            temp = temp.get_next()
+        print(temp.get_data())
+    
+    def remove(self, data):
+        # print(node.__data)
+        temp = self.__node
+        prev = self.__node
+
+        flag = False
+
+        if temp.get_data() == data:
+            self.__node = temp.get_next()
+            flag = True
+        else:
+            while temp.get_next() != None:
+                if temp.get_data() == data:
+                    prev.set_next(temp.get_next())
+                    flag = True
+                    break
+                prev = temp
+                temp = temp.get_next()
+
+            if temp.get_data() == data and flag==False:
+                prev.set_next(None)
+                flag = True
+        if flag:
+            print(f"{data} removed")
+        else:
+            print(f"{data} not found")
+
+def main():
+
+    l1 = Linked_List(Node(1))
+    # node = l1
+
+    l1.append(Node(2))
+    l1.append(Node(3))
+    l1.append(Node(4))
+    l1.append(Node(2))
+    l1.append(Node(5))
+    l1.append(Node(2))
+    l1.append(Node(2))
+    l1.append(Node(6))
+    l1.append(Node('hello'))
+
+    l1.print_list()
+
+    l1.remove(3)
+
+    l1.print_list()
+
+    l1.update_first(2, 10)
+
+    l1.print_list()
+
+    l1.update_all(2, 1001)
+
+    l1.print_list()
+
+    
+    # print(temp.get_data())
+        
+main()
